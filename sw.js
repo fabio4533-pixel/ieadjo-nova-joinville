554ec04166e6674cb41604de553aad8c0a2f88ca
fabio4533-pixel <fabio4533@gmail.com>
2026-08-19 17:08:39 -0300

    Refresh app cache for editable member profiles

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ieadjo-v2';
+const CACHE='ieadjo-v4';
 const ASSETS=['/','/index-1.html','/manifest.webmanifest','/igreja-hq.jpg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
